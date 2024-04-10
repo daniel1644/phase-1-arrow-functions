110b6626b830a46cf87bc94a0fc95eb0f8284b3c
danielbeyene <daniel.beyene@student.moringashcool.com>
2024-04-10 12:45:20 +0300

    Arrow Functions

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function(a, b) {
+    return a / b;
+  };
+  // Now you can call the divide function like this:
+  console.log(divide(2000, 100)); // Output: 20
+
+
+  const square = (x) => x * x;
+// Now you can call the square function like this:
+console.log(square(2)); // Output: 4
+
+
+
+const add = (a, b) => a + b;
+// Now you can call the add function like this:
+console.log(add(3, 4)); // Output: 7
